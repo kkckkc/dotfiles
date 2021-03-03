e3cfce14bb11d9b10d8b5ec1cfe1e3081c6668c5
Magnus Johansson <magnus.johansson@se.ibm.com>
2021-03-03 17:56:02 +0100

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ln -s .p10k.zsh $HOME/.p10k.zsh
-ln -s .tmux.conf $HOME/.tmux.conf
-ln -s .zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.p10k.zsh $HOME/.p10k.zsh
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
