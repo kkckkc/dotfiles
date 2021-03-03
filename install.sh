83864c574571aa1c5977a2421bb9089e76c43517
Magnus Johansson <magnus.johansson@se.ibm.com>
2021-03-03 19:26:15 +0100

    gitconfig

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 ln -s $HOME/dotfiles/.p10k.zsh $HOME/.p10k.zsh
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
